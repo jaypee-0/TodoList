7fc7f902cd7ff8c16b0169f0bdbb94ce08228ac9
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-05-19 08:26:07 +0100

    Fixed active bar

--- src/App.js
+++ src/App.js
@@ -5,74 +5,77 @@ import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome';
 
 function App() {
   const [todos, settodos] = useState({
-    id: "",
-    task: "", 
-    completed: false
-  })
-  const [TODO, setTODO] = useState([])
-  const [showall, setshowall] = useState(true)
-  const [active, setactive] = useState(true)
-  const [completedbar, setcompletedbar] = useState(false)
-  const [showinput, setshowinput] = useState(true)
+    id: '',
+    task: '',
+    completed: false,
+  });
+  const [TODO, setTODO] = useState([]);
+  const [showall, setshowall] = useState(true);
+  const [active, setactive] = useState(false);
+  const [completedbar, setcompletedbar] = useState(false);
+  const [showinput, setshowinput] = useState(true);
 
-  const addTodos =(todos) => {
+  const addTodos = (todos) => {
     setTODO([todos, ...TODO]);
-  }
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(todos.task.trim()) {
+    if (todos.task.trim()) {
       addTodos({ ...todos, id: uuidv4() });
-      settodos({ ...todos, task: ""});
+      settodos({ ...todos, task: '' });
     }
-  }
+  };
   const handleComplete = () => {
-    setshowinput(false)
-    setcompletedbar(true) 
-    setshowall(false)    
-  }
+    setshowinput(false);
+    setcompletedbar(true);
+    setshowall(false);
+    setactive(false);
+  };
   const handleAll = () => {
-    setshowinput(true)
-    setcompletedbar(false)
-    setshowall(true)
-  }
+    setshowinput(true);
+    setcompletedbar(false);
+    setshowall(true);
+    setactive(false);
+  };
   const handleActive = () => {
-    setshowinput(true)
-    setcompletedbar(false)
-    setactive(active)
-  }  
+    setshowinput(true);
+    setcompletedbar(false);
+    setactive(true);
+    setshowall(false);
+  };
   const handleCheckbox = (todo) => {
     setTODO(
       TODO.map((todos) => {
         if (todos.id === todo.id) {
-          return {...todos, completed: !todos.completed};
+          return { ...todos, completed: !todos.completed };
         }
         return todos;
       })
-    )
-  }
+    );
+  };
   const handleRemove = ({ id }) => {
     setTODO(TODO.filter((todo) => todo.id !== id));
-  }
-  const handleRemoveAll = ({id}) => {
+  };
+  const handleRemoveAll = ({ id }) => {
     setTODO(TODO.filter((todo) => todo.id === id));
-  }
+  };
 
-  const LOCAL_KEY = "todo-list";
+  const LOCAL_KEY = 'todo-list';
   useEffect(() => {
     const storetodo = JSON.parse(localStorage.getItem(LOCAL_KEY));
     if (storetodo) {
       setTODO(storetodo);
     }
   }, []);
-  
+
   useEffect(() => {
     localStorage.setItem(LOCAL_KEY, JSON.stringify(TODO));
-  }, [TODO])
-  
+  }, [TODO]);
+
   return (
-    <div id='App' className="container">
-      <header className="fs-2 fw-bold mt-5 mb-4 text-center m-auto mr-5">
-       Todo
+    <div id='App' className='container'>
+      <header className='fs-2 fw-bold mt-5 mb-4 text-center m-auto mr-5'>
+        Todo
       </header>
       <div className='col-11 col-md-7 m-auto'>
         <ul className='select pl-0 d-flex flex-row justify-content-between col-10 m-auto mb-0'>
@@ -81,63 +84,135 @@ function App() {
           <li onClick={handleComplete}>Completed</li>
         </ul>
         <hr className='mt-0' />
-        {showinput && 
-        <form className="d-flex mb-2 w-100"
-        onSubmit={handleSubmit}>          
-        <input 
-        className="w-100 py-2 px-3 me-2" 
-        value={todos.task}
-        onChange={(e) => {
-          settodos({...todos, task: e.target.value})
-        }}
-        type="text" 
-        placeholder='Enter todo' />
-        <button
-        className='py-2 px-4 add'>Add</button>
-      </form>
-        }  
-        {/* Todolists */} 
-        
-        <div className='d-flex flex-column'>  
-        {showall &&
-        <div className='d-flex flex-column'> 
-          {TODO.map(todo => {
-            return (
-              <ul className='carding list d-flex flex-row mb-3 pl-0 align-items-center justify-content-between' key={todo.id}>
-                <input type="checkbox" onClick={() => handleCheckbox(todo)} className='me-2 ms-3'/>
-                <li className={todo.completed ? 'text-decoration-line-through me-auto' : 'me-auto'}>{todo.task}</li>
-                <button type='submit' onClick={() => handleRemove(todo)} className='del py-0'><FA icon="fa-trash-can" beatFade="true" /></button>
-              </ul>
-            )
-          })
-          }
-        </div>
-        }    
+        {showinput && (
+          <form className='d-flex mb-2 w-100' onSubmit={handleSubmit}>
+            <input
+              className='w-100 py-2 px-3 me-2'
+              value={todos.task}
+              onChange={(e) => {
+                settodos({ ...todos, task: e.target.value });
+              }}
+              type='text'
+              placeholder='Enter todo'
+            />
+            <button className='py-2 px-4 add'>Add</button>
+          </form>
+        )}
+        {/* Todolists */}
+        <div className='d-flex flex-column'>
+          {showall && (
+            <div className='d-flex flex-column'>
+              {TODO.map((todo) => {
+                return (
+                  <ul
+                    className='carding list d-flex flex-row mb-3 pl-0 align-items-center justify-content-between'
+                    key={todo.id}>
+                    <input
+                      type='checkbox'
+                      onClick={() => handleCheckbox(todo)}
+                      className='me-2 ms-3'
+                    />
+                    <li
+                      className={
+                        todo.completed
+                          ? 'text-decoration-line-through me-auto'
+                          : 'me-auto'
+                      }>
+                      {todo.task}
+                    </li>
+                    <button
+                      type='submit'
+                      onClick={() => handleRemove(todo)}
+                      className='del py-0'>
+                      <FA icon='fa-trash-can' beatFade='true' />
+                    </button>
+                  </ul>
+                );
+              })}
+            </div>
+          )}
+          {/* Active Tab */}
+          {active && (
+            <div>
+              {TODO.filter((todo) => {
+                if (todo.completed === todos.completed) {
+                  return todos;
+                }
+                return todo.completed && todos.completed;
+              }).map((todo) => {
+                return (
+                  <ul
+                    className='carding list d-flex flex-row mb-3 pl-0 align-items-center justify-content-between'
+                    key={todo.id}>
+                    <input
+                      type='checkbox'
+                      onClick={() => handleCheckbox(todo)}
+                      className='me-2 ms-3'
+                    />
+                    <li
+                      className={
+                        todo.completed
+                          ? 'text-decoration-line-through me-auto'
+                          : 'me-auto'
+                      }>
+                      {todo.task}
+                    </li>
+                    <button
+                      type='submit'
+                      onClick={() => handleRemove(todo)}
+                      className='del py-0'>
+                      <FA icon='fa-trash-can' beatFade='true' />
+                    </button>
+                  </ul>
+                );
+              })}
+            </div>
+          )}
           {/* Complete Tab */}
-          {completedbar && 
-          <div>
-            {TODO.filter(todo => {
-              if (todo.completed !== todos.completed) {
-                return todos
-              } return (todo.completed && todos.completed)                
-              })
-              .map(todo => {
-              return (
-              <ul className='carding list d-flex flex-row mb-3 pl-0 align-items-center justify-content-between' key={todo.id}>
-                <input type="checkbox" onClick={() => handleCheckbox(todo)} className='me-2 ms-3'/>
-                <li className={todo.completed ? 'text-decoration-line-through me-auto' : 'me-auto'}>{todo.task}</li>
-                <button type='submit' onClick={() => handleRemove(todo)} className='del py-0'><FA icon="fa-trash-can" beatFade="true" /></button>
-              </ul>
-            )})
-            }
-          <div className='d-flex flex-row align-items-center mt-2'>
-            <button className='genDel ms-auto px-3 py-2'
-            onClick={handleRemoveAll}>Delete All</button>
-          </div>
-          </div>
-          }  
-            
-          </div>
+          {completedbar && (
+            <div>
+              {TODO.filter((todo) => {
+                if (todo.completed !== todos.completed) {
+                  return todos;
+                }
+                return todo.completed && todos.completed;
+              }).map((todo) => {
+                return (
+                  <ul
+                    className='carding list d-flex flex-row mb-3 pl-0 align-items-center justify-content-between'
+                    key={todo.id}>
+                    <input
+                      type='checkbox'
+                      onClick={() => handleCheckbox(todo)}
+                      className='me-2 ms-3'
+                    />
+                    <li
+                      className={
+                        todo.completed
+                          ? 'text-decoration-line-through me-auto'
+                          : 'me-auto'
+                      }>
+                      {todo.task}
+                    </li>
+                    <button
+                      type='submit'
+                      onClick={() => handleRemove(todo)}
+                      className='del py-0'>
+                      <FA icon='fa-trash-can' beatFade='true' />
+                    </button>
+                  </ul>
+                );
+              })}
+              <div className='d-flex flex-row align-items-center mt-2'>
+                <button
+                  className='genDel ms-auto px-3 py-2'
+                  onClick={handleRemoveAll}>
+                  Delete All
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
